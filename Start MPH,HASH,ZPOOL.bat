604ff65f41a037bff448971c03443f655f29a4c3
tutulino <tutulino@gmail.com>
2017-08-03 13:24:31 +0200

    FIX INTERVAL FROM CFG NOT WORKING

--- Start MPH,HASH,ZPOOL.bat
+++ Start MPH,HASH,ZPOOL.bat
@@ -3,5 +3,5 @@
 for /f "delims== tokens=1,2" %%G in (config.txt) do set %%G=%%H
 
 :LOOP
-powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\MultiPoolMiner.ps1 -interval 100 -Wallet %WALLET% -Username %USERNAME% -Workername %WORKERNAME% -Location US -PoolName zpool,hash,mph -Type %TYPE%  -Donate %DONATE% -CURRENCY %CURRENCY% -WalletDonate %WALLETDONATE%
+powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\MultiPoolMiner.ps1 -interval %INTERVAL% -Wallet %WALLET% -Username %USERNAME% -Workername %WORKERNAME% -Location US -PoolName zpool,hash_refinery,mining_pool_hub -Type %TYPE%  -Donate %DONATE% -CURRENCY %CURRENCY% -WalletDonate %WALLETDONATE%
 GOTO LOOP
--- Start MPH.bat
+++ Start MPH.bat
@@ -4,5 +4,5 @@ for /f "delims== tokens=1,2" %%G in (config.txt) do set %%G=%%H
 echo 
 
 :LOOP
-powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\MultiPoolMiner.ps1 -interval 100 -Wallet %WALLET% -Username %USERNAME% -Workername %WORKERNAME% -Location US -PoolName mph -Type %TYPE%  -Donate %DONATE% -WalletDonate %WALLETDONATE%
+powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\MultiPoolMiner.ps1 -interval %INTERVAL% -Wallet %WALLET% -Username %USERNAME% -Workername %WORKERNAME% -Location US -PoolName Mining_Pool_Hub -Type %TYPE%  -Donate %DONATE% -WalletDonate %WALLETDONATE%
 GOTO LOOP
