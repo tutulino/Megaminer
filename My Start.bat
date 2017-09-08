5dd1f85f4c3c6b28874aa38eef2cc62aea6814b1
Michael Busby <MikeBuzz@users.noreply.github.com>
2017-09-08 23:27:52 +0100

    Fixes to algo and startup

--- My Start.bat
+++ My Start.bat
@@ -6,9 +6,7 @@ setx GPU_SINGLE_ALLOC_PERCENT 100
 
 :LOOP
 
-nvidiaInspector.exe -setBaseClockOffset:0,0,200 -setMemoryClockOffset:0,0,600 -setPowerTarget:0,112 -setTempTarget:0,0,75 -setFanSpeed:0,70 -forcepstate:0,0
-
-del "Stats\*_Profit.txt"
+nvidiaInspector.exe -setBaseClockOffset:0,0,200 -setMemoryClockOffset:0,0,600 -setPowerTarget:0,112 -setTempTarget:0,0,75 -setFanSpeed:0,70
 
 powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\core.ps1 -MiningMode AUTOMATIC -PoolsName Mining_Pool_Hub
 GOTO LOOP
