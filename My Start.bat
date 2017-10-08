6fe14a435676dabcec7e468f6962a9bd72206962
Michael Busby <MikeBuzz@users.noreply.github.com>
2017-10-08 09:24:33 +0100

    Small clean up

--- My Start.bat
+++ My Start.bat
@@ -6,7 +6,7 @@ setx GPU_SINGLE_ALLOC_PERCENT 100
 
 :LOOP
 
-nvidiaInspector.exe -setBaseClockOffset:0,0,200 -setMemoryClockOffset:0,0,600 -setPowerTarget:0,112 -setTempTarget:0,0,80
+nvidiaInspector.exe -setBaseClockOffset:0,0,200 -setMemoryClockOffset:0,0,600 -setPowerTarget:0,112 -setTempTarget:0,0,75
 
 powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\core.ps1 -MiningMode AUTOMATIC -PoolsName Mining_Pool_Hub,Nicehash
 GOTO LOOP
