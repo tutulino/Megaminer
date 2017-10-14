870b0900aed843d85df934659843524befee629e
yuzi-co <vadim@yuzi.co>
2017-10-14 17:06:13 +0300

    add my configs

---
+++ AutoStart Example - Auto with algo filter.bat
@@ -0,0 +1,15 @@
+REM --This is an example of how launch Megaminer without prompt for automatic coin selection pools
+
+
+setx GPU_FORCE_64BIT_PTR 1
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+
+
+:LOOP
+del "Stats\*_Profit.txt"
+
+powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\core.ps1 -MiningMode Automatic -PoolsName Zpool  -Algorithm Lyra2z
+GOTO LOOP
---
+++ AutoStart.bat
@@ -0,0 +1,15 @@
+@echo off
+setx GPU_FORCE_64BIT_PTR 1
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+
+cd /d %~dp0
+
+:LOOP
+del "Stats\*_Profit.txt"
+
+powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\core.ps1 -MiningMode AUTOMATIC -PoolsName Zpool,Mining_Pool_Hub,NICEHASH,Hash_Refinery
+
+GOTO LOOP
