25b1a8b2d2f3580f336df91a31a920d3a1cc47e2
Michael Busby <MikeBuzz@users.noreply.github.com>
2017-12-08 14:25:54 +0000

    Corrected fee for Yiimp

--- Pools/YIIMP.ps1
+++ Pools/YIIMP.ps1
@@ -130,7 +130,7 @@ if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")){
                                 Blocks_24h    = $coin."24h_blocks"
                                 WalletMode    = $WalletMode
                                 PoolName = $Name
-                                Fee = $coin.fees/100
+                                Fee = 0.02
                                 }
                         
                 
