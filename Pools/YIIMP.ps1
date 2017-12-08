8a4bfd0c7d66daa467d025cac31ef205b2be38a4
Michael Busby <MikeBuzz@users.noreply.github.com>
2017-12-08 13:37:25 +0000

    Update YIIMP.ps1
    Bring back yiimp as the api now seems very stable

--- Pools/YIIMP.ps1
+++ Pools/YIIMP.ps1
@@ -1,4 +1,4 @@
-﻿param(
+param(
     [Parameter(Mandatory = $true)]
     [String]$Querymode = $null ,
     [Parameter(Mandatory = $false)]
@@ -9,10 +9,10 @@
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 $ActiveOnManualMode    = $true
-$ActiveOnAutomaticMode = $false
-$ActiveOnAutomatic24hMode = $false
+$ActiveOnAutomaticMode = $true
+$ActiveOnAutomatic24hMode = $true
 $AbbName = 'YIIMP'
-$WalletMode = "NONE"
+$WalletMode ='WALLET'
 $Result = @()
 
 
@@ -37,7 +37,37 @@ if ($Querymode -eq "info"){
 
 
 
-               
+
+
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+#****************************************************************************************************************************************************************************************
+
+
+
+    if ($Querymode -eq "wallet")    {
+        
+                            
+                            try {
+                                $http="http://api.yiimp.eu/api/wallet?address="+$Info.user
+                                $Yiimp_Request = Invoke-WebRequest -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"  $http -UseBasicParsing -timeoutsec 10 | ConvertFrom-Json 
+                            }
+                            catch {}
+        
+        
+                            if ($Yiimp_Request -ne $null -and $Yiimp_Request -ne ""){
+                                $Result = [PSCustomObject]@{
+                                                        Pool =$name
+                                                        currency = $Yiimp_Request.currency
+                                                        balance = $Yiimp_Request.balance
+                                                    }
+                                    remove-variable Yiimp_Request                                                                                                        
+                                    }
+
+                        
+                        }
+                        
+                        
 
 
 #****************************************************************************************************************************************************************************************
@@ -46,71 +76,67 @@ if ($Querymode -eq "info"){
     
 if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")){
 
-        $Pools=@()
-        $Pools +=[pscustomobject]@{"Symbol"="AUR"; "algo"="skein";"port"=4933;"coin"="AuroraCoin";"location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="BOAT"; "algo"="hmq1725";"port"=3747;"coin"="Doubloon";"location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="BSD";"algo"="xevan"; "port"=3739;"coin"="BitSend"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="BSTY"; "algo"="yescrypt";"port"=6233;"coin"="GlobalBoostY";  "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="BTX";"algo"="bitcore";"port"=3556;"coin"="BitCore";"location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="CHC";"algo"="c11";"port"=3573;"coin"="Chaincoin";"location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="DCR";"algo"="decred";"port"=3252;"coin"="decred"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="DGB";"algo"="skein";"port"=4933;"coin"="Digibyte";"location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="DNR";"algo"="tribus";"port"=8533;"coin"="Denarius"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="FTC";"algo"="neoscrypt";"port"=4233;"coin"="Feathercoin";"location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="GRS";"algo"="groestl";"port"=5339;"coin"="Groestlcoin"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="HUSH";"algo"="equihash";"port"=2142;"coin"="Hush"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="KMD";"algo"="equihash";"port"=2142;"coin"="Komodo"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="MAC";"algo"="timetravel";"port"=3555;"coin"="MachineCoin"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="NEVA";"algo"="blake2s";"port"=4262;"coin"="Neva"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="ORB";"algo"="neoscrypt";"port"=4233;"coin"="OrbitCoin"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="SIB";"algo"="sib";"port"=5033;"coin"="SibCoin";  "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="SIGT";"algo"="skunk";"port"=8433;"coin"="Signatum"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="SWEEP";"algo"="jha";"port"=4633;"coin"="Sweepstake"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="TAJ";"algo"="blake2s";"port"=4262;"coin"="TajCoin"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="TIT";"algo"="sha256";"port"=3333;"coin"="Titcoin"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="VIVO";"algo"="neoscrypt";"port"=4233;"coin"="Vivo"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="VTC";"algo"="lyra2v2";"port"=4533;"coin"="VertCoin"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="XLR";"algo"="nist5";"port"=3833;"coin"="Solaris"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="XRE";"algo"="x11evo";"port"=3553;"coin"="Revolver"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="XVG";"algo"="x17";"port"=3737;"coin"="Verge"; "location"="US";"server"="yiimp.ccminer.org"}
-        $Pools +=[pscustomobject]@{"Symbol"="ZEN";"algo"="equihash";"port"=2142;"coin"="ZenCash"; "location"="US";"server"="yiimp.ccminer.org"}
+        $retries=1
+                do {
+                        try {
+                            $Yiimp_Request = Invoke-WebRequest "http://api.yiimp.eu/api/currencies"  -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36" -UseBasicParsing -timeout 5 
+                            $Yiimp_Request = $Yiimp_Request | ConvertFrom-Json 
+                             #$Zpool_Request=get-content "..\zpool_request.json" | ConvertFrom-Json
+
+                        }
+                        catch {}
+                        $retries++
+                    if ($Yiimp_Request -eq $null -or $Yiimp_Request -eq "") {start-sleep 5}
+                    } while ($Yiimp_Request -eq $null -and $retries -le 3)
+                
+                if ($retries -gt 3) {
+                                    WRITE-HOST 'YIIMP API NOT RESPONDING...ABORTING'
+                                    EXIT
+                                    }
 
 
-        $Pools |  ForEach-Object {
+        $Yiimp_Request | Get-Member -MemberType properties| ForEach-Object {
 
-                    $Yiimp_Algorithm = get-algo-unified-name $_.algo
-                    $Yiimp_coin =  get-coin-unified-name $_.coin
-                    $Yiimp_symbol = $_.Symbol
+                $coin=$Yiimp_Request | Select-Object -ExpandProperty $_.name
+                
+
+                    $Yiimp_Algorithm = get-algo-unified-name $coin.algo
+                    $Yiimp_coin =  get-coin-unified-name $coin.name
+                    $Yiimp_Simbol=$_.name
+            
+
+                    $Divisor = Get-Algo-Divisor $Yiimp_Algorithm
+                    
                 
 
                     $Result+=[PSCustomObject]@{
                                 Algorithm     = $Yiimp_Algorithm
                                 Info          = $Yiimp_coin
-                                Price         = $null
-                                Price24h      = $null
+                                Price         = [Double]$coin.estimate / $Divisor
+                                Price24h      = [Double]$coin.actual_last24h / $Divisor
                                 Protocol      = "stratum+tcp"
-                                Host          = $_.server
-                                Port          = $_.port
-                                User          = $CoinsWallets.get_item($Yiimp_symbol)
-                                Pass          = "c=$Yiimp_symbol,ID=#WorkerName#"
-                                Location      = $_.location
+                                Host          = "yiimp.eu"
+                                Port          = $coin.port
+                                User          = $CoinsWallets.get_item($Yiimp_Simbol)
+                                Pass          = "c=$Yiimp_symbol,ID=$WorkerName"
+                                Location      = 'US'
                                 SSL           = $false
-                                Symbol        = $Yiimp_Symbol
+                                Symbol        = $Yiimp_Simbol
                                 AbbName       = $AbbName
                                 ActiveOnManualMode    = $ActiveOnManualMode
                                 ActiveOnAutomaticMode = $ActiveOnAutomaticMode
-                                PoolWorkers   = $_.Workers
-                                PoolHashRate  = $null
-                                Blocks_24h    = $null
+                                PoolWorkers       = $coin.Workers
+                                PoolHashRate  = $coin.HashRate
+                                Blocks_24h    = $coin."24h_blocks"
                                 WalletMode    = $WalletMode
                                 PoolName = $Name
-                                Fee = 0.02
+                                Fee = $coin.fees/100
                                 }
                         
                 
                 }
 
-  
+  remove-variable Yiimp_Request                
     }
 
 
@@ -120,4 +146,4 @@ if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")){
 
     $Result |ConvertTo-Json | Set-Content ("$name.tmp")
     remove-variable Result
-    
+  
