40eb98ce7cf96197dc59fcc428fe5bea58ca7ccb
tutulino <tutulino@gmail.com>
2018-02-22 10:32:27 +0100

    Added non main page coins

--- Pools/Whattomine.ps1
+++ Pools/Whattomine.ps1
@@ -90,8 +90,40 @@ if ($Querymode -eq "core"  -or $Querymode -eq "Menu"){
 
       
    #Data from WTM
+                $WTMResponse2=@()
+
+                #Add main page coins
                 try {$WTMResponse = Invoke-WebRequest "https://whattomine.com/coins.json" -UseBasicParsing -timeoutsec 10 | ConvertFrom-Json | Select-Object -ExpandProperty coins} catch { WRITE-HOST 'WTM API NOT RESPONDING...ABORTING';EXIT}
-         
+                $WTMResponse.psobject.properties.name  | ForEach-Object {
+
+                $res=$WTMResponse.($_) 
+                $res |add-member name $_
+                $WTMResponse2 += $res
+                }
+
+
+
+
+                try {$WTMResponse = Invoke-WebRequest "http://whattomine.com/calculators.json" -UseBasicParsing -timeoutsec 10 | ConvertFrom-Json | Select-Object -ExpandProperty coins } catch { WRITE-HOST 'WTM API NOT RESPONDING...ABORTING';EXIT}
+
+
+                #Add secondary page coins
+
+                $WTMResponse.psobject.properties.name | ForEach-Object {
+
+                        if ($WTMResponse.($_).Status -eq "Active") {
+                        $Id = $WTMResponse.($_).Id
+                        $exists= $WTMResponse2 | Where-Object id -eq $Id
+                        if ($exists.count -eq 0) {
+                                $page="https://whattomine.com/coins/"+$WTMResponse.($_).Id+".json"
+                                try {$WTMResponse2 += Invoke-WebRequest $page -UseBasicParsing -timeoutsec 2 | ConvertFrom-Json  } catch {}
+                                }
+                        }
+                }
+                
+
+
+                        
         #search on pools where to mine coins, order is determined by config.txt @@WHATTOMINEPOOLORDER variable
                 $ConfigOrder = (get_config_variable "WHATTOMINEPOOLORDER") -split ','
                 foreach ($PoolToSearch in $ConfigOrder)  {
@@ -101,25 +133,25 @@ if ($Querymode -eq "core"  -or $Querymode -eq "Menu"){
                                 #Filter by minworkes variable (must be here for not selecting now a pool and after that discarded on core.ps1 filter)
                                 $HPools = $HPools | Where-Object {$_.Poolworkers -ge (get_config_variable "MINWORKERS") -or $_.Poolworkers -eq $null}
         
-                                ForEach ($WtmCoinName in $WTMResponse.psobject.properties.name)  {
+                                ForEach ($WtmCoinName in $WTMResponse2)  {
 
-                                        $Algorithm=get_algo_unified_name ($WTMResponse.($WtmCoinName).Algorithm)
-                                        $Coin= get_coin_unified_name $WtmCoinName
+                                        $Algorithm=get_algo_unified_name ($WtmCoinName.Algorithm)
+                                        $Coin= get_coin_unified_name ($WtmCoinName.name)
 
                                         #search if this coin was added before
                                         if (($Result | where-object { $_.Info -eq $coin -and  $_.Algorithm -eq $Algorithm}).count -eq 0)  {
                                             $Hpool = $HPools | where-object { $_.Info -eq $coin -and  $_.Algorithm -eq $Algorithm}
                                             if ($Hpool -ne $null) { #Search if each pool has coin correspondence in WTM        
 
-                                                $WTMFactor = get_WhattomineFactor ($Hpool.Algorithm)
+                                                $WTMFactor = get_WhattomineFactor $Algorithm
 
                                                 if ($WTMFactor -ne $null) {
                                                                 $Result +=[pscustomobject]@{
-                                                                        Info            = $Hpool.Info
-                                                                        Algorithm       = $Hpool.Algorithm
-                                                                        Price           = [Double]($WTMResponse.($_.coin).btc_revenue/$WTMFactor)
-                                                                        Price24h        = [Double]($WTMResponse.($_.coin).btc_revenue24/$WTMFactor)
-                                                                        symbol          = $WTMResponse.($Hpool.Info).tag
+                                                                        Info            = $Coin
+                                                                        Algorithm       = $Algorithm
+                                                                        Price           = [Double]($WtmCoinName.btc_revenue/$WTMFactor)
+                                                                        Price24h        = [Double]($WtmCoinName.btc_revenue24/$WTMFactor)
+                                                                        symbol          = $WtmCoinName.tag
                                                                         Host            = $Hpool.host
                                                                         port            = $Hpool.port
                                                                         location        = $Hpool.location
