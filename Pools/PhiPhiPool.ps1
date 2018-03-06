0cbc2210efdb0410e4b11ec3987a351f20551789
yuzi-co <vadim@yuzi.co>
2018-03-06 22:06:51 +0200

    add PhiPhiPool

---
+++ Pools/PhiPhiPool.ps1
@@ -0,0 +1,150 @@
+﻿param(
+    [Parameter(Mandatory = $true)]
+    [String]$Querymode = $null,
+    [Parameter(Mandatory = $false)]
+    [pscustomobject]$Info
+)
+
+#. .\..\Include.ps1
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+$ActiveOnManualMode = $true
+$ActiveOnAutomaticMode = $true
+$ActiveOnAutomatic24hMode = $true
+$AbbName = 'PHI'
+$WalletMode = 'WALLET'
+$ApiUrl = 'http://www.phi-phi-pool.com/api'
+$MineUrl = 'pool1.phi-phi-pool.com'
+$Location = 'US'
+$RewardType = "PPS"
+$UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
+$Result = @()
+
+
+if ($Querymode -eq "info") {
+    $Result = [PSCustomObject]@{
+        Disclaimer               = "Autoexchange to BTC or any coin on the pool, no registration required"
+        ActiveOnManualMode       = $ActiveOnManualMode
+        ActiveOnAutomaticMode    = $ActiveOnAutomaticMode
+        ActiveOnAutomatic24hMode = $ActiveOnAutomatic24hMode
+        ApiData                  = $True
+        AbbName                  = $AbbName
+        WalletMode               = $WalletMode
+        RewardType               = $RewardType
+    }
+}
+
+
+if ($Querymode -eq "speed") {
+    try {
+        $http = $ApiUrl + "/walletEx?address=" + $Info.user
+        $Request = Invoke-WebRequest -UserAgent $UserAgent $http -UseBasicParsing -timeoutsec 5 | ConvertFrom-Json
+    } catch {}
+
+    $Result = @()
+
+    if (![string]::IsNullOrEmpty($Request)) {
+        $Request.Miners |ForEach-Object {
+            $Result += [PSCustomObject]@{
+                PoolName   = $name
+                Version    = $_.version
+                Algorithm  = get_algo_unified_name $_.Algo
+                Workername = $_.password.Split(",")[1].Split('=')[1]
+                Diff       = $_.difficulty
+                Rejected   = $_.rejected
+                Hashrate   = $_.accepted
+            }
+        }
+        remove-variable Request
+    }
+}
+
+
+if ($Querymode -eq "wallet") {
+    try {
+        $http = $ApiUrl + "/wallet?address=" + $Info.user
+        $Request = Invoke-WebRequest $http -UserAgent $UserAgent -UseBasicParsing -timeoutsec 5 | ConvertFrom-Json
+    } catch {}
+
+    if (![string]::IsNullOrEmpty($Request)) {
+        $Result = [PSCustomObject]@{
+            Pool     = $name
+            currency = $Request.currency
+            balance  = $Request.balance
+        }
+        remove-variable Request
+    }
+}
+
+
+if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")) {
+    $retries = 1
+    do {
+        try {
+            $http = $ApiUrl + "/status"
+            $Request = Invoke-WebRequest $http -UserAgent $UserAgent -UseBasicParsing -timeoutsec 5 | ConvertFrom-Json
+        } catch {start-sleep 2}
+        $retries++
+        if ([string]::IsNullOrEmpty($Request)) {start-sleep 3}
+    } while ($Request -eq $null -and $retries -le 3)
+
+    if ($retries -gt 3) {
+        Write-Host $Name 'API NOT RESPONDING...ABORTING'
+        Exit
+    }
+
+
+    $Currency = if ([string]::IsNullOrEmpty($(get_config_variable "CURRENCY_$Name"))) { get_config_variable "CURRENCY" } else { get_config_variable "CURRENCY_$Name" }
+
+    $Request | Get-Member -MemberType Properties | ForEach-Object {
+
+        $coin = $Request | Select-Object -ExpandProperty $_.name
+        $Pool_Algo = get_algo_unified_name $coin.name
+
+        $Divisor = 1000000
+
+        switch ($Pool_Algo) {
+            "Blake2s" {$Divisor *= 1000}
+            "Blakecoin" {$Divisor *= 1000}
+            "Decred" {$Divisor *= 1000}
+            "Equihash" {$Divisor /= 1000}
+            "Keccak" {$Divisor *= 1000}
+            "Quark" {$Divisor *= 1000}
+            "Qubit" {$Divisor *= 1000}
+            "Scrypt" {$Divisor *= 1000}
+            "X11" {$Divisor *= 1000}
+        }
+
+        if ($coin.actual_last24h -gt 0 -and $coin.hashrate -gt 0 -and $coin.Workers -gt 0) {
+            $Result += [PSCustomObject]@{
+                Algorithm             = $Pool_Algo
+                Info                  = $Pool_Algo
+                Price                 = $coin.estimate_current / $Divisor
+                Price24h              = $coin.estimate_last24h / $Divisor
+                Protocol              = "stratum+tcp"
+                Host                  = $coin.name + "." + $MineUrl
+                Port                  = $coin.port
+                User                  = $CoinsWallets.get_item($Currency)
+                Pass                  = "c=$Currency,ID=#Workername#"
+                Location              = $Location
+                SSL                   = $false
+                Symbol                = get_coin_symbol -Coin $Pool_Algo
+                AbbName               = $AbbName
+                ActiveOnManualMode    = $ActiveOnManualMode
+                ActiveOnAutomaticMode = $ActiveOnAutomaticMode
+                PoolWorkers           = $coin.Workers
+                PoolHashRate          = $coin.hashrate
+                WalletMode            = $WalletMode
+                WalletSymbol          = $Currency
+                PoolName              = $Name
+                Fee                   = $coin.Fees / 100
+                RewardType            = $RewardType
+            }
+        }
+    }
+    remove-variable Request
+}
+
+
+$Result |ConvertTo-Json | Set-Content $info.SharedFile
+remove-variable Result
