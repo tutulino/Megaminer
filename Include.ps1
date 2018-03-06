9700f20472010712a50b02010e5332627443eaad
tutulino <tutulino@gmail.com>
2018-03-06 13:42:04 +0100

    added phoenixminer

--- Include.ps1
+++ Include.ps1
@@ -802,7 +802,16 @@ function Get_Live_HashRate {
                                     $HashRate_Dual = [double]$Data.result[4].Split(";")[0] * $Multiplier
                                     }
 
-            }
+                        }
+            "phoenix" {
+
+                        $Parameters = @{id = 1; jsonrpc = "2.0"; method = "miner_getstat1"} | ConvertTo-Json  -Compress
+                        $Request = Invoke_tcpRequest $Server $Port $Parameters 5
+                        if ($Request -ne "" -and $request -ne $null) {
+                            $Data = $Request | ConvertFrom-Json
+                            $HashRate =[int](($Data.result[2] -split ';')[0]) * 1000
+                            }
+                        }
 
             "ClaymoreV2" {
 
