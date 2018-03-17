bea3109078ac58c352b41d1236f7e62d6557288b
tutulino <tutulino@gmail.com>
2018-03-17 20:14:18 +0100

    removed enviroment AMD variables

--- AutoStart Example - one coin manual.bat
+++ AutoStart Example - one coin manual.bat
@@ -1,17 +1,6 @@
 REM --This is an example of how launch Megaminer without prompt for automatic coin selection pools
 
 cd %~dp0
-
-setx GPU_FORCE_64BIT_PTR 1
-setx GPU_MAX_HEAP_SIZE 100
-setx GPU_USE_SYNC_OBJECTS 1
-setx GPU_MAX_ALLOC_PERCENT 100
-setx GPU_SINGLE_ALLOC_PERCENT 100
-
-
-
 :LOOP
-del "Stats\*_Profit.txt"
-
 powershell -version 5.0 -noexit -executionpolicy bypass -command "&.\core.ps1 -MiningMode Manual -PoolsName suprnova -Coinsname Bitcore
 GOTO LOOP
