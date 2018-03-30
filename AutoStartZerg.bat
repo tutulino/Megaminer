da8f64c1d8400aeabad7e282900b589121ac78b4
yuzi-co <vadim@yuzi.co>
2018-03-31 01:58:00 +0300

    dont use PS6 by default

--- AutoStartZerg.bat
+++ AutoStartZerg.bat
@@ -4,9 +4,7 @@ cd /d %~dp0
 
 set "command=& .\core.ps1 -MiningMode Automatic -PoolsName ZergPool"
 
-pwsh -noexit -executionpolicy bypass -command "%command%"
 powershell -version 5.0 -noexit -executionpolicy bypass -command "%command%"
-msiexec -i https://github.com/PowerShell/PowerShell/releases/download/v6.0.2/PowerShell-6.0.2-win-x64.msi -qb!
-pwsh -noexit -executionpolicy bypass -command "%command%"
+::pwsh -noexit -executionpolicy bypass -command "%command%"
 
 pause
