86ac6359b2bc3238045eeee3a613a6111d341e38
yuzi-co <vadim@yuzi.co>
2018-04-01 16:44:20 +0300

    update WTM pool

--- Pools/WhatToMine.ps1
+++ Pools/WhatToMine.ps1
@@ -68,7 +68,23 @@ if ($Querymode -eq "core" -or $Querymode -eq "Menu") {
     #Common Data from WTM
 
     #Add main page coins
-    $WTMResponse = Invoke_APIRequest -Url 'https://whattomine.com/coins.json' -Retry 3 | Select-Object -ExpandProperty coins
+    $WtmUrl = 'https://whattomine.com/coins.json?' +
+    'eth=true&factor[eth_hr]=10&factor[eth_p]=0&' + #Ethash
+    'grof=true&factor[gro_hr]=10&factor[gro_p]=0&' + #Groestl
+    'x11gf=true&factor[x11g_hr]=10&factor[x11g_p]=0&' + #X11gost
+    'cn=true&factor[cn_hr]=10&factor[cn_p]=0&' + #CryptoNight
+    'eq=true&factor[eq_hr]=10&factor[eq_p]=0&' + #Equihash
+    'lre=true&factor[lrev2_hr]=10&factor[lrev2_p]=0&' + #Lyra2v2
+    'ns=true&factor[ns_hr]=10&factor[ns_p]=0&' + #NeoScrypt
+    'lbry=true&factor[lbry_hr]=10&factor[lbry_p]=0&' + #Lbry
+    'bk14=true&factor[bk14_hr]=10&factor[bk14_p]=0&' + #Decred
+    'pas=true&factor[pas_hr]=10&factor[pas_p]=0&' + #Pascal
+    'skh=true&factor[skh_hr]=10&factor[skh_p]=0&' + #Skunk
+    'n5=true&factor[n5_hr]=10&factor[n5_p]=0&' + #Nist5
+    'l2z=true&factor[l2z_hr]=10&factor[l2z_p]=0&' + #Lyra2z
+    'xn=true&factor[xn_hr]=10&factor[xn_p]=0' #Xevan
+
+    $WTMResponse = Invoke_APIRequest -Url $WtmUrl -Retry 3 | Select-Object -ExpandProperty coins
     if (!$WTMResponse) {
         Write-Host $Name 'API NOT RESPONDING...ABORTING'
         Exit
@@ -104,28 +120,26 @@ if ($Querymode -eq "core" -or $Querymode -eq "Menu") {
 
         #we must add units for each algo, this value must be filled if we want a coin to be selected
         $WTMFactor = switch ($HPool.Algorithm) {
-            #main page
-            "Ethash" { 84000000 }
-            "Sib" { 20100000 }
-            "CryptoNight" { 2190 }
-            "Equihash" { 870 }
-            "Lyra2v2" { 14700000 }
-            "NeoScrypt" { 2460000 }
-            "Skunk" { 54000000 }
-            "Nist5" { 57000000 }
-
-            #others
-            "Bitcore" { 30000000 }
-            "Blake2s" { 7500000000 }
-            "CryptoLight" { 6600 }
-            "Keccak" { 900000000 }
-            "KeccakC" { 240000000 }
-            "Lyra2z" { 420000 }
-            "X17" { 100000 }
-            "Xevan" { 4800000 }
-            "Yescrypt" { 13080 }
-            "Zero" { 18 }
-            default {$null}
+            "Bitcore" { 1000000 }
+            "Blake2s" { 1000000 }
+            "CryptoLight" { 1 }
+            "CryptoNight" { 1 }
+            "Equihash" { 1 }
+            "Ethash" { 1000000 }
+            "Keccak" { 1000000 }
+            "KeccakC" { 1000000 }
+            "Lyra2v2" {1000}
+            "Lyra2z" { 1000 }
+            "NeoScrypt" { 1000 }
+            "Nist5" { 1000000 }
+            "PHI" { 1000000 }
+            "Sib" { 1000000 }
+            "Skunk" { 1000000 }
+            "X16r" { 1000000 }
+            "X17" { 1000 }
+            "Yescrypt" { 1 }
+            "Zero" { 1 }
+            default { $null }
         }
 
         if (($Result | Where-Object { $_.Info -eq $HPool.Info -and $_.Algorithm -eq $HPool.Algorithm}).count -eq 0 -and $WTMFactor) {
@@ -144,16 +158,26 @@ if ($Querymode -eq "core" -or $Querymode -eq "Menu") {
                     $_.Algorithm -eq $HPool.Algorithm
                 }
                 if ($WtmSecCoin) {
-                    $WtmCoin = Invoke_APIRequest -Url $('https://whattomine.com/coins/' + $WtmSecCoin.Id + '.json') -Retry 3
-                    if ($WtmCoin) {$WtmCoin | Add-Member btc_revenue24 $WtmCoin.btc_revenue}
+                    $WtmCoin = Invoke_APIRequest -Url ('https://whattomine.com/coins/' + $WtmSecCoin.Id + '.json?hr=10&p=0&fee=0.0&cost=0.0&hcost=0.0') -Retry 3
+                    if ($WtmCoin) {
+                        if (![decimal]$WtmCoin.btc_revenue) {
+                            if (!$CMCResponse) {
+                                'Calling CoinMarketCap API' | Write-Host
+                                $CMCResponse = Invoke_APIRequest -Url "https://api.coinmarketcap.com/v1/ticker/?limit=0" -MaxAge 60 -Retry 1
+                            }
+                            $CMCPrice = [decimal]($CMCResponse | Where-Object Symbol -eq $WtmCoin.tag | Select-Object -First 1 -ExpandProperty price_btc)
+                            $WtmCoin.btc_revenue = $CMCPrice * [decimal]$WtmCoin.estimated_rewards
+                        }
+                        $WtmCoin | Add-Member btc_revenue24 $WtmCoin.btc_revenue
+                    }
                 }
             }
-            if ($WtmCoin -ne $null) {
+            if ($WtmCoin) {
                 $Result += [PSCustomObject]@{
                     Info                  = $HPool.Info
                     Algorithm             = $HPool.Algorithm
-                    Price                 = [decimal]$WtmCoin.btc_revenue / $WTMFactor
-                    Price24h              = [decimal]$WtmCoin.btc_revenue24 / $WTMFactor
+                    Price                 = [decimal]$WtmCoin.btc_revenue / $WTMFactor / 10
+                    Price24h              = [decimal]$WtmCoin.btc_revenue24 / $WTMFactor / 10
                     Symbol                = $WtmCoin.Tag
                     Host                  = $HPool.Host
                     HostSSL               = $HPool.HostSSL
