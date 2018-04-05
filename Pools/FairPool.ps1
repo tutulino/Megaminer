127712de2a358dc0e4ffe92640fee4b1dfdfd0f3
yuzi-co <vadim@yuzi.co>
2018-04-05 22:05:53 +0300

    Cryptonight update hell preparatation... to be continued

--- Pools/FairPool.ps1
+++ Pools/FairPool.ps1
@@ -69,7 +69,7 @@ if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")) {
     $Pools += [pscustomobject]@{"coin" = "EthereumClassic"; "algo" = "Ethash"; "symbol" = "ETC"; "server" = "mine.etc.fairpool.cloud"; "port" = 4444; "fee" = 0.01}
     $Pools += [pscustomobject]@{"coin" = "Metaverse"; "algo" = "Ethash"; "symbol" = "ETP"; "server" = "mine.etp.fairpool.cloud"; "port" = 6666; "fee" = 0.01}
     $Pools += [pscustomobject]@{"coin" = "PascalLite"; "algo" = "Pascal"; "symbol" = "PASL"; "server" = "mine.pasl.fairpool.cloud"; "port" = 4009; "fee" = 0.02}
-    $Pools += [pscustomobject]@{"coin" = "Sumokoin"; "algo" = "CryptoNight"; "symbol" = "SUMO"; "server" = "mine.sumo.fairpool.cloud"; "port" = 5555; "fee" = 0.01}
+    $Pools += [pscustomobject]@{"coin" = "Sumokoin"; "algo" = "CryptoNightHeavy"; "symbol" = "SUMO"; "server" = "mine.sumo.fairpool.cloud"; "port" = 5555; "fee" = 0.01}
 
     $Pools | ForEach-Object {
         if ($CoinsWallets.($_.symbol)) {
