fa3f9992404b4763b8dccd8e6acb8d92b45114ca
yuzi-co <vadim@yuzi.co>
2018-04-11 19:38:54 +0300

    fix bug with failed benchmarks

--- core.ps1
+++ core.ps1
@@ -1075,7 +1075,7 @@ while ($Quit -eq $false) {
             -Value $ActiveMiners[$BestNow.IdF].SubMiners[$BestNow.Id].StatsHistory
     }
 
-    if ($ActiveMiners.SubMiners | Where-Object {$_.NeedBenchmark -and $_.Status -ne 'Cancelled'}) {$NeedBenchmark = $true} else {$NeedBenchmark = $false}
+    if ($ActiveMiners.SubMiners | Where-Object {$_.NeedBenchmark -and $_.Status -notin @('Cancelled', 'Failed')}) {$NeedBenchmark = $true} else {$NeedBenchmark = $false}
 
     if ($DonationInterval) { $NextInterval = $DonateInterval }
     elseif ($NeedBenchmark) { $NextInterval = $BenchmarkIntervalTime }
