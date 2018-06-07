ed8ea121f9dcacb978a890bc4585240a74815b58
yuzi-co <vadim@yuzi.co>
2018-06-07 20:21:00 +0300

    cleanup

--- AutoStart Example - Two pools on Auto.bat
+++ AutoStart Example - Two pools on Auto.bat
@@ -2,7 +2,7 @@
 
 cd /d %~dp0
 
-set "command=& .\core.ps1 -MiningMode Automatic -PoolsName MiningPoolHub,YIIMP"
+set "command=& .\core.ps1 -MiningMode Automatic -PoolsName MiningPoolHub,NiceHash"
 
 powershell -version 5.0 -noexit -executionpolicy bypass -command "%command%"
 ::pwsh -noexit -executionpolicy bypass -command "%command%"
