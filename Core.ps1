b93489685b915871dfeb1792c3ce222548bc7867
yuzi-co <vadim@yuzi.co>
2018-08-13 16:52:28 +0300

    v18.08.2

--- Core.ps1
+++ Core.ps1
@@ -79,7 +79,7 @@ $ErrorActionPreference = "Continue"
 $Config = Get-Config
 
 $Application = "Forager"
-$Release = "18.08.1"
+$Release = "18.08.2"
 Log-Message "$Application v$Release"
 
 $Host.UI.RawUI.WindowTitle = "$Application v$Release"
