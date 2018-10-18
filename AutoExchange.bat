57e0458dd835058150458bfecddaeefbf19cf32e
yuzi-co <vadim@yuzi.co>
2018-10-18 15:54:25 +0300

    remove Zerg from sample autostart

--- AutoExchange.bat
+++ AutoExchange.bat
@@ -3,7 +3,7 @@
 cd /d %~dp0
 
 set Mode=Automatic
-set Pools=MiningPoolHub,NiceHash,Zpool,ZergPool,BlockMasters
+set Pools=MiningPoolHub,NiceHash,Zpool,BlockMasters
 
 powershell -version 5.0 -noexit -executionpolicy bypass -command "& .\Core.ps1 -MiningMode %Mode% -PoolsName %Pools%"
 ::pwsh -noexit -executionpolicy bypass -command "& .\Core.ps1 -MiningMode %Mode% -PoolsName %Pools%"
