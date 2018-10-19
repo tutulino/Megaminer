0488d054692fc0366247417bbe117fdb7d4f8b4e
yuzi-co <vadim@yuzi.co>
2018-10-19 20:24:30 +0300

    fix Monero on MPH

--- Pools/MiningPoolHub.ps1
+++ Pools/MiningPoolHub.ps1
@@ -107,7 +107,7 @@ if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")) {
 
             $Server = $MiningPoolHub_Hosts | Sort-Object {$_ -like "$Location*"} -Descending | Select-Object -First 1
 
-            $enableSSL = (('CnV7', 'Equihash') -contains $MiningPoolHub_Algorithm)
+            $enableSSL = (('CnV8', 'Equihash') -contains $MiningPoolHub_Algorithm)
 
             $Result += [PSCustomObject]@{
                 Algorithm             = $MiningPoolHub_Algorithm
